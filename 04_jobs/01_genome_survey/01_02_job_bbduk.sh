bf3dbca38b9fac9ddf590aabb633d3dc4063b35c
zpqu <quzp2006@gmail.com>
2022-06-24 10:43:35 +0930

    Added v1 trim job

---
+++ 04_jobs/01_genome_survey/01_02_job_bbduk.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -N 1
+#SBATCH -n 16 
+#Job requires 32 tasks (32 cores)
+#SBATCH --time=24:00:00
+#SBATCH --mem=32GB
+#SBATCH --ntasks-per-core=2
+
+#SBATCH --mail-type=END
+#SBATCH --mail-user=zhipeng.qu@adelaide.edu.au
+#SBATCH -o 01_02_job_bbduk.slurm-%j.log
+
+#load module
+module load Java
+
+cd ../../03_scripts/01_genome_survey/
+
+./01_02_run_bbduk.sh >01_02_run_bbduk.20220623.log 2>&1
+
+sacct -j $SLURM_JOBID --format="JobID,JobName,Partition,ReqNodes,ReqCPUS,ReqMem,Timelimit,ExitCode,Cluster,Account,AllocCPUS,AllocNodes,CPUTime,MaxRSS,Elapsed,MaxDiskRead,MaxDiskWrite,Submit,Start"
